23f2d4e33c34f41079e6a77bcdd3288aec29c815
smail <smail@karasky>
2019-06-30 23:15:06 +0100

    te

---
+++ db-service/src/main/java/com/smailelfathi/dbservice/repository/QuotesRepository.java
@@ -0,0 +1,11 @@
+package com.smailelfathi.dbservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smailelfathi.dbservice.model.Quote;
+
+public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+    List<Quote> findByUserName(String username);
+}
