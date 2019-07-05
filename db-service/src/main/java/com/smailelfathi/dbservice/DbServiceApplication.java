0a8087b2ba614d877159a210ce4712599943ac93
selfathi <smail.el.fathi@gmail.com>
2019-07-05 14:28:36 +0100

    hystrix

--- db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
@@ -3,15 +3,34 @@ package com.smailelfathi.dbservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages ="com.smailelfathi.dbservice.repository")
 @SpringBootApplication
+@EnableHystrix
+@RestController
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbServiceApplication.class, args);
 	}
+	
+	@RequestMapping(value = "/")
+	   @HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
+	      @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+	   })
+	   public String hello() throws InterruptedException {
+	      Thread.sleep(3000);
+	      return "Welcome Hystrix";
+	   }
+	   private String fallback_hello() {
+	      return "Request fails. It takes long time to response";
+	   }
 
 }
