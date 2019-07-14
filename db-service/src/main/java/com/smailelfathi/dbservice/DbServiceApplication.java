69b5b57d8426c6bcc1b0bb7a97bf17884ecdb35a
smail <smail@karasky>
2019-07-14 14:06:06 +0100

    cleaning

--- db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
@@ -5,16 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages ="com.smailelfathi.dbservice.repository")
 @SpringBootApplication
 @EnableHystrix
-@EnableHystrixDashboard
 @EnableCircuitBreaker
+@RestController
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
@@ -22,5 +26,20 @@ public class DbServiceApplication {
 	}
 	
 	
+	   @RequestMapping(value = "/")
+	   @HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
+	      @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+	   })
+	   public String hello() throws InterruptedException {
+		   if(Math.random()>.5) {
+	      Thread.sleep(3000);
+	      throw new RuntimeException("RuntimeException");
+		   }
+	      return "Welcome Hystrix";
+	   }
+	   
+	   private String fallback_hello() {
+	      return "Request fails. It takes long time to response";
+	   }
 
 }
