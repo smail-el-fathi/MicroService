e95623c24739be87713123a26596c645d3c93f0e
smail <smail.el.fathi@gmail.com>
2020-06-14 22:08:26 +0100

    config server

---
+++ config-service/src/test/java/com/smailelfathi/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smailelfathi.configservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
