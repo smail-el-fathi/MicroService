f64cbb57e6e899f7ae7744e78b64ae305a636ccf
smail <smail.el.fathi@gmail.com>
2020-06-15 21:37:15 +0100

    clean up

--- db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/smailelfathi/dbservice/DbServiceApplication.java
@@ -1,11 +1,17 @@
 package com.smailelfathi.dbservice;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +25,29 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 @EnableHystrix
 @EnableCircuitBreaker
 @RestController
+@RefreshScope
 public class DbServiceApplication {
+	
+	
+	@Value("${xParam}")
+	private Boolean xParam;
+	
+	@Value("${copyRight}")
+	private String copyRight;
+	
+	@Value("${spring.jpa.show-sql}")
+	private Boolean showSQL;
+	
+	@GetMapping("/myConfig")
+	public Map<String,Object> Myconfig(){
+		 Map<String,Object> params=new HashMap<>();
+		 params.put("xParam",xParam);
+		 params.put("copyRight",copyRight);
+		 params.put("showSQL",showSQL);
 
+		return params;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DbServiceApplication.class, args);
 	}
