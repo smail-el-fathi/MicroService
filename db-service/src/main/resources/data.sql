f66f90940a2e9d46a4771f6f16f8f9e2857e87c4
selfathi <smail.elfathi@smart-rx.com>
2020-10-18 14:53:48 +0100

    change the quotes messgae

--- db-service/src/main/resources/data.sql
+++ db-service/src/main/resources/data.sql
@@ -1,5 +1,14 @@
 insert into quotes
-values(10001,'asmaa quote', 'asmaa');
+values(10001,'we learn and grow together', 'quote1');
 
 insert into quotes
-values(10002,'smail quote', 'smail');
+values(10002,' If you can’t build a well-structured monolith, what makes you think you can build a well-structured set of microservices?', 'quote2');
+
+
+
+insert into quotes
+values(10003,'If you are working in an organization that places lots of restrictions on how developers can do their work, then microservices may not be for you.', 'quote3');
+
+insert into quotes
+values(10004,'can you make a change to a service and deploy it by itself without changing anything else? If the answer is no, then many of the advantages we discuss throughout this presentation will be hard for you to achieve.', 'quote4');
+
